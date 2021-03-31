ca26b38589473ad8f6b76e35f3098d8cffa66182
Amberas <amber.aspeslagh@student.howest.be>
2021-03-31 09:13:49 +0200

    aanpassingen

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "cb64a9fb293f7912c3ce";
+/******/ 	var hotCurrentHash = "f57aa5d80079a0b6538c";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
